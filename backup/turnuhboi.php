e6d1d17236095f5fe238d042515131f5ca20b08c
shane1027 <shanend27@gmail.com>
2015-10-31 20:08:05 -0400

    added actual script file

--- backup/turnuhboi.php
+++ backup/turnuhboi.php
@@ -1,31 +1,31 @@
 <?php
 
-require_once("/home/shane/builds/UniChat/SC-API/src/snapchat.php");
+require_once("../src/snapchat.php");
 
 //////////// CONFIG ////////////
-$username       = " "; // Your snapchat username
-$password       = " "; // Your snapchat password
-$gEmail         = " "; // Gmail account
-$gPasswd        = " "; // Gmail account password
-$casperKey      = " "; // Casper API Key
-$casperSecret   = " "; // Casper API Secret
+$username       = "turnuhboi"; // Your snapchat username
+$password       = "turntoi1738"; // Your snapchat password
+$gEmail         = "unichat10@gmail.com"; // Gmail account
+$gPasswd        = "Irish1027"; // Gmail account password
+$casperKey      = "cf487d25a9d9232f5e10f6ca2533114b"; // Casper API Key
+$casperSecret   = "b9f4251c079a162bfbcc585939432e2e"; // Casper API Secret
 $debug = false; // Set this to true if you want to see all outgoing requests and responses from server
 ////////////////////////////////
 
 
-// $imagePath = "...pic.jpeg"; // URL or local path to a media file (image or video)
+$imagePath = "/home/shane/builds/UniChat/pic.jpeg"; // URL or local path to a media file (image or video)
+// $sendTo = array("actionjaxon195", "ceuell14", "cmward1996", "ian.gaus", "jdisser", "jkrivda", "mattrrrr", "meg_hunt", "petethemeat", "phealings", "ryan.shane", "jjbyrne1414", "onesgnation");
 $sendTo = array("ryan.shane");
 
 $snapchat = new Snapchat($username, $gEmail, $gPasswd, $casperKey, $casperSecret, $debug);
 
-$snapchat->openAppEvent();
+// $snapchat->openAppEvent();
 // $snapchat->addFriend($sendTo);
 
 //Login to Snapchat with your username and password
-//must use for first login!
-//$snapchat->login($password);
+$snapchat->login($password);
 
-// $snapchat->addFriend("wmorgenlander");
+$snapchat->addFriend("petethemeat");
 
 // Get friends in an array
 $friends = $snapchat->getFriends();
@@ -54,31 +54,28 @@ for ($i = 0; $i < count($receivedData); $i++) {
 }
 print_r($receivedUsers);
 
-$dir = "/home/shane/builds/UniChat/SC-API/src/";          
+$dir = "/home/shane/builds/UniChat/vendor/mgp25/SC-API/src/";          
                                                                        
-foreach (glob($dir ."snaps/turnuhboi/*/*") as $files) {            
-    $snapchat->setStory($files, 8);                                 
-    // $snapchat->send($files, "ryan.shane");
+foreach (glob($dir ."snaps/turnuhboi/*/*.jpg") as $files) {            
+    // $snapchat->setStory($files, 8);                                 
+    $snapchat->send($files, "ryan.shane");
     echo "$files size " . filesize($files) . "\n";                     
 } 
                                                                        
 foreach (glob($dir ."stories/*/*/*") as $files) {            
     // $snapchat->setStory($files, 8);                              
-    // $snapchat->send($files, "ryan.shane");
+    $snapchat->send($files, "ryan.shane");
     echo "$files size " . filesize($files) . "\n";                     
 } 
 
 // Download stories for a specific username
-$snapchat->getStoriesByUsername("meg_hunt", true);
+$snapchat->getStoriesByUsername("meg_hunt, ryan.shane", true);
 
 // Send chat message to shane to ensure server is updating
-$snapchat->sendMessage($sendTo, "Story updated.");
+$snapchat->sendMessage($sendTo, "Story updated." . " turn sloich!");
 
 // send message to users who submitted snaps to confirm
 // $snapchat->sendMessage($receivedUsers, "I put your snap up on my story." . $phrases[rand()]);
-$snapchat->sendMessage($receivedUsers, "I put your snap up on my story.");
-
-$snapchat->clearFeed();
 
 $snapchat->closeAppEvent();
 
